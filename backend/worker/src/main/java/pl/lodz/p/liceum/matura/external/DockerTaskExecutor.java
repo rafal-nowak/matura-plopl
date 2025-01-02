187ccc88ed74d50203ced254015b3bbfd248881a
rafal-nowak <contact@rafal-nowak.com>
2025-01-02 13:04:03 +0100

    experiments

--- backend/worker/src/main/java/pl/lodz/p/liceum/matura/external/DockerTaskExecutor.java
+++ backend/worker/src/main/java/pl/lodz/p/liceum/matura/external/DockerTaskExecutor.java
@@ -155,6 +155,7 @@ public class DockerTaskExecutor implements TaskExecutor {
         dockerComposeGenerator.generate(
                 subtask.getWorkspaceUrl() + "/docker-compose.yml",
                 taskDefinition.getEnvironment(),
+                taskDefinition.getLimits(),
                 checkData
         );
 
