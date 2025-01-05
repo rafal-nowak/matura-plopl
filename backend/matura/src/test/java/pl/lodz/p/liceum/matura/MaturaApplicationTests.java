28c65fa1adc2e2e25fad9aa97e6226e7a13b513f
rafal-nowak <contact@rafal-nowak.com>
2025-01-05 21:28:36 +0100

    file locking experiment

--- backend/matura/src/test/java/pl/lodz/p/liceum/matura/MaturaApplicationTests.java
+++ backend/matura/src/test/java/pl/lodz/p/liceum/matura/MaturaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MaturaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
