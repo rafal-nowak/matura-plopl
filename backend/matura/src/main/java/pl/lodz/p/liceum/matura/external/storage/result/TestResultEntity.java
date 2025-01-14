9da32679d0f2b6ae608b767313932a844e86f6f2
Hubert <h.makowski66@gmail.com>
2025-01-14 19:05:35 +0100

    Added TestResult

---
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/external/storage/result/TestResultEntity.java
@@ -0,0 +1,54 @@
+package pl.lodz.p.liceum.matura.external.storage.result;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pl.lodz.p.liceum.matura.domain.result.Verdict;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+public class TestResultEntity {
+    @Id
+    @SequenceGenerator(
+            name = "result_id_seq",
+            sequenceName = "result_id_seq",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "result_id_seq"
+    )
+    private Integer id;
+
+    @Column(name="subtask_result_id", nullable = false)
+    private Integer subtaskResultId;
+    @Column(name="verdict", nullable = false)
+    private Verdict verdict;
+    @Column(name="time", nullable = false)
+    private Integer time;
+    @Column(name="memory", nullable = false)
+    private Integer memory;
+    @Column(name="message", nullable = false, columnDefinition = "TEXT")
+    private String message;
+    @Column(name="created_at", nullable = false)
+    private ZonedDateTime createdAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestResultEntity that = (TestResultEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+}
---
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/external/storage/result/TestResultStorageAdapter.java
@@ -0,0 +1,53 @@
+package pl.lodz.p.liceum.matura.external.storage.result;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.dao.DataIntegrityViolationException;
+import pl.lodz.p.liceum.matura.domain.result.ResultAlreadyExistsException;
+import pl.lodz.p.liceum.matura.domain.result.TestResult;
+import pl.lodz.p.liceum.matura.domain.result.TestResultRepository;
+import pl.lodz.p.liceum.matura.domain.result.TestResultRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Log
+public class TestResultStorageAdapter implements TestResultRepository {
+
+    private final JpaTestResultRepository repository;
+    private final TestResultEntityMapper mapper;
+
+    @Override
+    public TestResult save(final TestResult testResult) {
+        try {
+            var a = mapper.toEntity(testResult);
+            TestResultEntity saved = repository.save(mapper.toEntity(testResult));
+            log.info("Saved entity " + saved);
+            return mapper.toDomain(saved);
+        } catch (DataIntegrityViolationException e) {
+            log.warning("TestResult " + testResult.getId() + " already exists");
+            throw new ResultAlreadyExistsException();
+        }
+    }
+
+    @Override
+    public void update(final TestResult testResult) {
+        repository.findById(testResult.getId()).ifPresent(taskEntity -> repository.save(mapper.toEntity(testResult)));
+    }
+
+    @Override
+    public void remove(final Integer id) {
+        repository.findById(id).ifPresent(resultEntity -> repository.deleteById(id));
+    }
+
+    @Override
+    public Optional<TestResult> findById(final Integer id) {
+        return repository.findById(id).map(mapper::toDomain);
+    }
+
+    @Override
+    public List<TestResult> findBySubtaskResultId(final Integer subtaskResultId) {
+        return repository.findBySubtaskResultId(subtaskResultId).stream().map(mapper::toDomain).toList();
+    }
+}
