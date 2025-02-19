26c5b666db08eda8c8939d47b9e65859882c7591
Hubert <h.makowski66@gmail.com>
2025-02-19 23:12:32 +0100

    Working on communication with the main application and fixing bugs

--- backend/matura/src/main/java/pl/lodz/p/liceum/matura/external/worker/task/events/SubtaskSentForFullProcessingEvent.java
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/external/worker/task/events/SubtaskSentForFullProcessingEvent.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class SubtaskSentForFullProcessingEvent extends SubtaskEvent {
+public class SubtaskSentForFullProcessingEvent extends TaskEvent {
     Integer taskId;
     Integer submissionId;
     String workspaceUrl;
