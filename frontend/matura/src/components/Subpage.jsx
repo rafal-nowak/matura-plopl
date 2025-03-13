39eb5066f584856bb27b46b249c4dd8996d50677
Wygraniec <wojciech.legocki@gmail.com>
2025-03-13 23:21:35 +0100

    Fixed the bug with minHeight in Subpage.jsx

--- frontend/matura/src/components/Subpage.jsx
+++ frontend/matura/src/components/Subpage.jsx
@@ -6,7 +6,7 @@ export const Subpage = ({children}) => (
     <>
         <Navbar/>
 
-        <Box paddingX='20px' paddingY='10px' margin='10px' minHeight='90dvh'>
+        <Box paddingX='20px' paddingY='10px' margin='10px' minHeight='80dvh'>
             {children}
         </Box>
     </>
