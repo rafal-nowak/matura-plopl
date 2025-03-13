0f8884c1dd2dd15a52bb60da1cfce31dac841fad
Wygraniec <wojciech.legocki@gmail.com>
2025-03-13 16:19:41 +0100

    Added temporary redirect

--- frontend/matura/src/Dashboard.jsx
+++ frontend/matura/src/Dashboard.jsx
@@ -2,6 +2,8 @@ import {withAuthentication} from "./routeAuthentication.jsx";
 import {Navbar} from "./components/Navbar.jsx";
 
 const Dashboard = () => {
+    window.location = '/tasks';
+
     return (
         <>
             <Navbar/>
