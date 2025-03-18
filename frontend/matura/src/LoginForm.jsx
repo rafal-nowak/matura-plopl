ea47a97b56baba31ed73ed18b83b75605fffb7dc
Wygraniec <wojciech.legocki@gmail.com>
2025-03-19 00:16:41 +0100

    Fixed the responsiveness of the login and registration forms and added button to switch between them

--- frontend/matura/src/LoginForm.jsx
+++ frontend/matura/src/LoginForm.jsx
@@ -13,29 +13,28 @@ import {
     Alert,
     useToast
 } from "@chakra-ui/react";
-import {Form, Formik, useField} from "formik";
+import { Form, Formik, useField } from "formik";
 import PropTypes from "prop-types";
 import * as Yup from "yup";
-import {useState} from "react";
-import {login, User} from "./services/userService.js";
-import {Navigate, useNavigate} from "react-router-dom";
+import { useState } from "react";
+import { login, User } from "./services/userService.js";
+import { Navigate, useNavigate } from "react-router-dom";
 
-const InputField = ({label, type, ...props}) => {
+const InputField = ({ label, type, ...props }) => {
     const [field, meta] = useField(props);
-
     const [showPassword, setShowPassword] = useState(false);
 
     return (
-        <FormControl id={props.name}>
+        <FormControl id={props.name} mb={4}>
             <FormLabel htmlFor={props.name}>{label}</FormLabel>
             <Flex flexDirection='row'>
                 <Input
-                    borderTopRightRadius={type === 'password' ? '0' : 'auto'}
-                    borderBottomRightRadius={type === 'password' ? '0' : 'auto'}
+                    borderTopRightRadius={type === 'password' ? '0' : 'md'}
+                    borderBottomRightRadius={type === 'password' ? '0' : 'md'}
                     {...field}
                     {...props}
                     type={type !== 'password' ? type : showPassword ? 'text' : 'password'}
-                    autoComplete='1'
+                    autoComplete='on'
                 />
                 {type === 'password' && (
                     <Button
@@ -44,31 +43,30 @@ const InputField = ({label, type, ...props}) => {
                         onClick={() => setShowPassword(!showPassword)}
                     >
                         {showPassword ? (
-                            <i className="fa-solid fa-eye-slash"/>
+                            <i className="fa-solid fa-eye-slash" />
                         ) : (
-                            <i className="fa-solid fa-eye"/>
+                            <i className="fa-solid fa-eye" />
                         )}
                     </Button>
                 )}
             </Flex>
-            {
-                meta.touched && meta.error ? (
-                    <Alert className='error' status='error' mt='2'>
-                        <AlertIcon/>
-                        {meta.error}
-                    </Alert>
-                ) : null
-            }
+            {meta.touched && meta.error && (
+                <Alert className='error' status='error' mt='2'>
+                    <AlertIcon />
+                    {meta.error}
+                </Alert>
+            )}
         </FormControl>
-    )
-}
+    );
+};
+
 InputField.propTypes = {
-    label: PropTypes.object,
+    label: PropTypes.node,
     name: PropTypes.string.isRequired,
     type: PropTypes.string,
     placeholder: PropTypes.string,
     id: PropTypes.string
-}
+};
 
 export const LoginForm = () => {
     let navigate = useNavigate();
@@ -76,34 +74,24 @@ export const LoginForm = () => {
 
     try {
         if (User.fromLocalStorage() !== null && User.fromLocalStorage().validate())
-            return <Navigate to='/dashboard'/>
+            return <Navigate to='/dashboard' />;
     } catch (e) { /* empty */ }
 
     return (
         <Formik
-            initialValues={{email: '', password: ''}}
-
+            initialValues={{ email: '', password: '' }}
             validationSchema={
                 Yup.object({
-                    email: Yup
-                        .string()
-                        .email("Podana wartość musi być poprawnym adresem email")
-                        .required("Pole nie może być puste"),
-
-                    password: Yup
-                        .string()
-                        .required("Pole nie może być puste")
+                    email: Yup.string().email("Podana wartość musi być poprawnym adresem email").required("Pole nie może być puste"),
+                    password: Yup.string().required("Pole nie może być puste")
                 })
             }
-
             validateOnMount={true}
 
-            onSubmit={(values, {setSubmitting}) => {
+            onSubmit={(values, { setSubmitting }) => {
                 setSubmitting(true);
                 login(values.email, values.password)
-                    .then(() => {
-                        navigate('/dashboard');
-                    })
+                    .then(() => navigate('/dashboard'))
                     .catch(() => {
                         toast({
                             title: "Błąd logowania",
@@ -111,51 +99,50 @@ export const LoginForm = () => {
                             status: 'error',
                             duration: 8000,
                             isClosable: true,
-                        })
+                        });
                     })
-                    .finally(() => {
-                        setSubmitting(false)
-                    })
+                    .finally(() => setSubmitting(false));
             }}>
 
-            {({isValid, isSubmitting}) => (
+            {({ isValid, isSubmitting }) => (
                 <Form>
-                    <Flex justifyContent="center" alignItems="center" height="100vh">
-                        <Card variant='elevated' minWidth='50dvw'>
-                            <CardHeader>
+                    <Flex justifyContent="center" alignItems="center" height="100vh" p={4}>
+                        <Card variant='elevated' width={['100%', '100%', '400px']} padding='25px' boxShadow='xl' borderRadius='xl'>
+                            <CardHeader textAlign='center'>
                                 <Heading>Logowanie</Heading>
                             </CardHeader>
 
                             <CardBody>
-                                <Stack>
+                                <Stack spacing={4}>
+                                    <InputField name={'email'} label={<><i className="fa-solid fa-envelope" /> Adres email:</>} type='email' />
+                                    <InputField name={'password'} label={<><i className="fa-solid fa-lock" /> Hasło:</>} type='password' />
 
-                                    <InputField name={'email'}
-                                                label={<><i className="fa-solid fa-envelope"/> Adres email:</>}
-                                                type='email'/>
+                                    <Button
+                                        type='submit'
+                                        marginY={'10px'}
+                                        colorScheme='blue'
+                                        disabled={!isValid || isSubmitting}
+                                        isLoading={isSubmitting}
+                                        loadingText={'Zaloguj się'}
+                                    >
+                                        Zaloguj się
+                                    </Button>
 
-                                    <InputField name={'password'} label={<><i className="fa-solid fa-lock"/> Hasło:</>}
-                                                type='password'/>
-
+                                    <Button
+                                        colorScheme='green'
+                                        variant='outline'
+                                        onClick={() => navigate('/register')}
+                                    >
+                                        Nie masz konta? Zarejestruj się
+                                    </Button>
                                 </Stack>
-
-                                <Button
-                                    type='submit'
-                                    marginY={'25px'}
-                                    colorScheme='blue'
-                                    disabled={!isValid || isSubmitting}
-                                    isLoading={isSubmitting}
-                                    loadingText={'Zaloguj się'}
-                                >
-                                    Zaloguj się
-                                </Button>
-
                             </CardBody>
                         </Card>
                     </Flex>
                 </Form>
             )}
         </Formik>
-    )
-}
+    );
+};
 
-export default LoginForm
+export default LoginForm;
