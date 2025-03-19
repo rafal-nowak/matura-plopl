3b995eec0f278e2e4ad11dd01ab470888d23eb5c
Wygraniec <wojciech.legocki@gmail.com>
2025-03-19 01:14:06 +0100

    Fixed some warnings

--- frontend/matura/src/RegistrationForm.jsx
+++ frontend/matura/src/RegistrationForm.jsx
@@ -76,6 +76,7 @@ export const RegistrationForm = () => {
     try {
         if (User.fromLocalStorage() !== null && User.fromLocalStorage().validate())
             return <Navigate to='/dashboard' />;
+        // eslint-disable-next-line no-unused-vars
     } catch (e) { /* empty */ }
 
     return (
