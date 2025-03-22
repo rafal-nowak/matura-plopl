5c784c57ccad1caee213f39116016a86a959cbda
rafal-nowak <rafal.nowak.phd@gmail.com>
2025-03-22 15:49:32 +0100

    new frontend deployment

--- frontend/matura/vite.config.js
+++ frontend/matura/vite.config.js
@@ -3,11 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',           // Nasłuchuj na wszystkich interfejsach
-    port: 5173,                // Domyślny port Vite
-    strictPort: true,          // Jeśli port jest zajęty, nie zmienia automatycznie
-    allowedHosts: ['test.matura-frontend.it4u.app'],  // Lista dozwolonych hostów
+  preview: {
+    port: 8080,
+    strictPort: true,
   },
-})
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  },
+});
