636c5db3ecfb9d6a997e71a51d948d42a20ca1f8
Wygraniec <wojciech.legocki@gmail.com>
2025-03-23 20:54:52 +0100

    Added link to survey

--- frontend/matura/vite.config.js
+++ frontend/matura/vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig({
   base: "/",
   plugins: [react()],
   preview: {
-    port: 8080,
+    port: 5173,
     strictPort: true,
   },
   server: {
-    port: 8080,
+    port: 5173,
     strictPort: true,
     host: true,
-    origin: "http://0.0.0.0:8080",
+    origin: "http://0.0.0.0:5173",
     allowedHosts: ['test.matura-frontend.it4u.app'],
   },
 });
