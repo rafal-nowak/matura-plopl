fe73a6a2774d731364e1628f296e5446bb963c4e
Wygraniec <wojciech.legocki@gmail.com>
2025-03-23 23:05:34 +0100

    Restored @HubertM6's changes

--- backend/matura/src/main/java/pl/lodz/p/liceum/matura/domain/submission/SubmissionRepository.java
+++ backend/matura/src/main/java/pl/lodz/p/liceum/matura/domain/submission/SubmissionRepository.java
@@ -2,6 +2,7 @@ package pl.lodz.p.liceum.matura.domain.submission;
 
 import pl.lodz.p.liceum.matura.domain.subtask.Subtask;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubmissionRepository {
@@ -11,6 +12,7 @@ public interface SubmissionRepository {
 
     void remove(Integer id);
 
-
     Optional<Submission> findById(Integer id);
+
+    List<Submission> findByTaskId(Integer taskId);
 }
